aec26c24a5096878e2c426ceddf4f6a746529248
阿部直也 <n.abe@gemcook.com>
2020-05-27 16:50:35 +0900

    Override post method of the loginAPI

--- restframework-todo/todo_api/views.py
+++ restframework-todo/todo_api/views.py
@@ -4,7 +4,9 @@ from rest_framework import viewsets
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.settings import api_settings
+from rest_framework.response import Response
 
 from todo_api import serializers
 from todo_api import models
@@ -24,6 +26,12 @@ class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+    def post(self, request, *args, **kwargs):
+        """Return user_id with token"""
+        response = super(UserLoginApiView, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'id': token.user_id})
+
 
 class TodoItemViewSet(viewsets.ModelViewSet):
     """Handles creating, reading and updating todo items"""
@@ -36,7 +44,3 @@ class TodoItemViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
         serializer.save(user=self.request.user)
-
-
-class AuthInfoGetView(generics.RetrieveAPIView):
-    """Retrieve the logged in user information"""
